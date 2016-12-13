4f507c4b53c16414d4a032fcaac6e0b5aa407f8a
Gentilhomme <gentilhomme.thomas@gmail.com>
2016-12-13 12:31:22 +0100

    Table exemple

---
+++ archive_inventory.sql
@@ -0,0 +1,11 @@
+CREATE TABLE NIMSOFT_hub_packages (
+    hubs VARCHAR(255)
+    name VARCHAR(255)
+    description VARCHAR(255)
+    version VARCHAR(255)
+    build VARCHAR(255)
+    date DATE
+    author VARCHAR(255)
+    pkg_group VARCHAR(255)
+    inserted DATETIME
+)
